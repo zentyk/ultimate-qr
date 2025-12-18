7470c1ae8d4614dd59a34d1a04670efc3a883d6c
artur <zentyk@protonmail.com>
2025-12-18 13:22:41 -0600

    PROJECT/SETUP

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import QRLogic from './components/QRLogic.vue'
+import PWABadge from './components/PWABadge.vue'
+</script>
+
+<template>
+  <QRLogic></QRLogic>
+  <PWABadge />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/QRLogic.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import {Html5QrcodeScanner} from "html5-qrcode";
+
+let copyButton:HTMLElement | null;
+let resultDom:HTMLElement | null;
+
+function onScanSuccess(decodedText:string) {
+  if(resultDom){
+    resultDom.innerText = decodedText;
+  }
+}
+
+function onScanFailure(error:any) {
+  console.warn(`Code scan error = ${error}`);
+}
+
+const CopyResult = () => {
+  let textToCopy = resultDom?.innerText?resultDom.innerText:"";
+
+  navigator.clipboard.writeText(textToCopy).then(()=>{
+    if(copyButton===null) return;
+
+    if ("innerText" in copyButton) {
+      copyButton.innerText = "Copied ✔️";
+    }
+    setTimeout(()=>{
+      if(copyButton===null) return;
+      if ("innerText" in copyButton) {
+        copyButton.innerText = "Copiar";
+      }
+    },2000);
+  })
+}
+
+document.addEventListener("DOMContentLoaded",function (){
+  let html5QrcodeScanner = new Html5QrcodeScanner(
+      "reader",
+      { fps: 10, qrbox: {width: 250, height: 250} },
+      /* verbose= */ false);
+  html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+
+  resultDom =document.getElementById('text');
+  copyButton = document.getElementById("CopyBtn");
+})
+</script>
+
+<template>
+<div id="reader" width="600px"></div>
+  <div id="text"></div>
+  <button id="CopyBtn" @click="CopyResult">Copy</button>
+</template>
+
+<style scoped>
+</style>
